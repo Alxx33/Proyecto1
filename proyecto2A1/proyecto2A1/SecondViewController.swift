aa028b3a0af6997f2b8af4f2eaad4f37ab38b966
Alxx33 <30987675+Alxx33@users.noreply.github.com>
2018-10-12 15:57:14 -0500

    Add files via upload

---
+++ proyecto2A1/proyecto2A1/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  proyecto2A1
+//
+//  Created by Laboratorio UNAM-Apple 04 on 12/10/18.
+//  Copyright © 2018 unam fca. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
